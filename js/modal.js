3fe1334461adf4021b73e7a44f040365a192963a
SamAlJuMa <samuelaljuma@gmail.com>
2019-10-28 14:04:12 -0600

    agregue pagina de usuario y correccion de bugs

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,19 @@ var span=document.getElementById("close");
 
 var img = document.getElementsByClassName('gal-img');
 var modalImg = document.getElementById("modal-test");
-img[0].onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-}
 
 document.addEventListener('click', function (event) {
 
 	if (event.target.matches('.gal-img')) {
     modal.style.display = "block";
     modalImg.src = event.target.src;
+    if(modalImg.height > modalImg.width){
+      modalImg.style.width="50%";
+    }
+    else {
+      modalImg.style.width="100%";
+      modalImg.style.height="auto";
+    }
 	}
 
 
