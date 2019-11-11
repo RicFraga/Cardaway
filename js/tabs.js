1a3ef7d4553bc769ae2ca026fc7f56363668f014
SamAlJuMa <samuelaljuma@gmail.com>
2019-11-11 01:21:16 -0600

    Página de usuario

--- js/tabs.js
+++ js/tabs.js
@@ -1,3 +1,19 @@
-$(document).ready(function () {
+var tabContent;
+tabcontent = document.getElementsByClassName("tabContent");
+for (i = 1; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
 
-})
+function openTab(evt, tabName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" selected", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " selected";
+}
