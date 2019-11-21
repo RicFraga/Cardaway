eecab65bb28d3a169a07f58822db9ab21c813770
Ricardo <rokoneitor@gmail.com>
2019-11-20 18:28:10 -0600

    semana anterior

--- php/functions/categorias.php
+++ php/functions/categorias.php
@@ -4,7 +4,9 @@
     $consulta = "
     Select cat.nombre nom, count(cat.nombre) tot
     From envios env, categorias cat, postales post
-    Where cat.id_categoria = post.id_categoria and post.id_postal=env.id_postal    
+    Where cat.id_categoria = post.id_categoria and post.id_postal=env.id_postal
+    And fecha_hora >= curdate() - INTERVAL DAYOFWEEK(curdate())+6 DAY
+    And fecha_hora < curdate() - INTERVAL DAYOFWEEK(curdate())-1 DAY
     Group by cat.nombre
     Order by tot Desc
     Limit 4
