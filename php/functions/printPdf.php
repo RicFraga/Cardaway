f80726f650e9f0378bde01932a7be75d67c4f3c5
LuisAldo <47335110+LuisAldo@users.noreply.github.com>
2019-11-27 13:54:48 -0600

    27/11/2019

--- php/functions/printPdf.php
+++ php/functions/printPdf.php
@@ -2,14 +2,24 @@
           
             require_once "./../../composer/vendor/autoload.php";
 
-            use Spipu\Htmlp2Pdf\Html2Pdf;
+            use Spipu\Html2Pdf\Html2Pdf;
+            use Spipu\Html2Pdf\Exception\Html2PdfException;
+            use Spipu\Html2Pdf\Exception\ExceptionFormatter;
 
-            ob_start();
-            require_once './email_template.php';
-            $html = ob_get_clean();
+            try{
+              ob_start();
+              require_once './email_template.php';
+              $html = ob_get_clean();
 
-            $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
-            $html2pdf->witeHTML($html);
-            $html2pdf->output(postal.pdf);
+              $html2pdf = new Html2Pdf('P', 'A4', 'es');
+              
+              $html2pdf->writeHTML($html);
+              $html2pdf->output(postal.pdf);
+            }catch(Html2PdfException $e){
+              $html2pdf->clean();
+
+              $formatter = new ExceptionFormatter($e);
+              echo $formatter->getHtmlMessage();
+            }
 
           ?>
